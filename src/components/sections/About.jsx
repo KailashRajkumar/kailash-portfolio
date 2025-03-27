04e530b91b6ff2581af1b4b8b7c922f01d444a78
KailashRajkumar <kailash14021999@gmail.com>
2025-03-27 12:37:16 +0530

    About Section Changed

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { FcBriefcase, FcLibrary } from 'react-icons/fc';
-import AOS from '../AnimationOnScroll';
+import RevealOnScroll from '../RevealOnScroll';
 
 const About = () => {
     const frontendSkills = [
@@ -17,8 +17,7 @@ const About = () => {
     ];
     return (
         <section id='about' className='min-h-screen flex items-center justify-center py-20'>
-            <AOS animation='' delay={0} duration={1000}>
-
+            <RevealOnScroll>
                 <div className='max-w-3xl mx-auto px-4'>
                     <h2 className='text-3xl font-bold mb-8 bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center'>
                         {" "}
@@ -40,8 +39,8 @@ const About = () => {
                                         <span
                                             key={key}
                                             className='bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
-                                    hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition
-                                    '>
+                                        hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition
+                                        '>
                                             {tech}
                                         </span>
                                     ))}
@@ -54,8 +53,8 @@ const About = () => {
                                         <span
                                             key={key}
                                             className='bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 
-                                    hover:shadow-[0_2px_8px_rgba(59,130,2246,0.1)] transition
-                                    '>
+                                        hover:shadow-[0_2px_8px_rgba(59,130,2246,0.1)] transition
+                                        '>
                                             {tech}
                                         </span>
                                     ))}
@@ -113,7 +112,7 @@ const About = () => {
                         </div>
                     </div>
                 </div>
-            </AOS>
+            </RevealOnScroll>
         </section >
     )
 }
